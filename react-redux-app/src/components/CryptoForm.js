70ae74fca6b3a7f9288b994b1ab70aea7d76a53f
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-02-06 23:29:26 -0500

    added Material UI styling to the button

--- react-redux-app/src/components/CryptoForm.js
+++ react-redux-app/src/components/CryptoForm.js
@@ -1,7 +1,62 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { green } from '@material-ui/core/colors';
+
 import { getData } from '../actions';
+
+
+const BootstrapButton = withStyles({
+  root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#007bff',
+    borderColor: '#007bff',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
 
 const CryptoForm = props => {
   const handleGetData = e => {
@@ -9,13 +64,23 @@ const CryptoForm = props => {
     props.getData();
   };
 
+  const classes = useStyles();
   return (
     <>
       {props.isFetchingData ? (
         <div>--We are retreiving your data--</div>
       ) : (
-        <button onClick={handleGetData}>Show me the $$$</button>
-      )}
+          <ThemeProvider theme={theme}>
+            <Button
+              onClick={handleGetData}
+              variant="contained"
+              color="primary"
+              className={classes.margin}
+            >Show me the $$$
+            </Button>
+          </ThemeProvider>
+          // <button onClick={handleGetData}>Show me the $$$</button>
+        )}
     </>
   );
 };
