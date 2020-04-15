7ee09edcb8c27350b59fe4cd7f242044d2a31f0b
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-04-14 20:52:54 -0400

    created reducer file

--- react-redux-app/src/actions/index.js
+++ react-redux-app/src/actions/index.js
@@ -5,6 +5,7 @@ export const UPDATE_CRYPTO = 'UPDATE_CRYPTO';
 export const SET_ERROR = 'SET_ERROR';
 
 export const getData = () => dispatch => {
+  
   dispatch({ type: FETCH_DATA });
   axios
     .get(
